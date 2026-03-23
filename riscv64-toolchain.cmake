b162613e019948b4f4a5dc16391d421b25d977fc
issaimaru <takeuchiissai2355@gmail.com>
2026-03-24 00:21:42 +0900

    risc-v用にクロスコンパイルするためのcmakeを追加

---
+++ riscv64-toolchain.cmake
@@ -0,0 +1,13 @@
+# RISC-V cross-compilation toolchain file
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR riscv64)
+
+# Compilers
+set(CMAKE_C_COMPILER riscv64-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER riscv64-linux-gnu-g++)
+
+# Search path
+set(CMAKE_FIND_ROOT_PATH /usr/lib/riscv64-linux-gnu)
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
