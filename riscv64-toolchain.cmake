d626b4becec5ef0b8bf310fc3ad1eabf2b2e9955
issaimaru <takeuchiissai2355@gmail.com>
2026-04-15 22:13:12 +0900

    br_realを直接指定

--- riscv64-toolchain.cmake
+++ riscv64-toolchain.cmake
@@ -5,8 +5,11 @@ set(CMAKE_SYSTEM_PROCESSOR riscv64)
 set(SDK_PATH "${CMAKE_CURRENT_LIST_DIR}/riscv-sdk/host")
 
 # Compilers
-set(CMAKE_C_COMPILER "${SDK_PATH}/bin/riscv64-buildroot-linux-gnu-gcc")
-set(CMAKE_CXX_COMPILER "${SDK_PATH}/bin/riscv64-buildroot-linux-gnu-g++")
+# Use the real compiler binaries directly. Some CMake versions resolve symlinks
+# and end up invoking toolchain-wrapper by its real name, which then looks for
+# non-existent toolchain-wrapper.br_real.
+set(CMAKE_C_COMPILER "${SDK_PATH}/bin/riscv64-buildroot-linux-gnu-gcc.br_real")
+set(CMAKE_CXX_COMPILER "${SDK_PATH}/bin/riscv64-buildroot-linux-gnu-g++.br_real")
 
 # imacのみでビルドする
 set(RISCV_FLAGS "-march=rv64imac -mabi=lp64")
